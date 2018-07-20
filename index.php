6c8641e229152fd3f681a467e83d35a940e854fd
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-07-20 18:29:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php include ('config.php')?>
 <html>
-<head>
+<!-- <head>
   <title>Heroku Test</title>
-</head>
+</head> -->
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+<!--<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
   <a class="navbar-brand" href="<?php echo ROOT_URL; ?>">Heroku Test</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
   </button>
 
-</nav>
+</nav>-->
 
 <?php
 echo 'Hello World';
